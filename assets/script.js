1ada9a4949fb190c248c2f639e03b9145e105357
kassrojas <kassandrarojass@gmail.com>
2022-12-09 09:51:58 -0500

    attempted to getAPI

--- assets/script.js
+++ assets/script.js
@@ -1,103 +1,21 @@
-'https://www.loc.gov/search/?q=' + q + '&fo=json';
-'https://www.loc.gov/' + format + '/?q=' + q + '&fo=json';
+'https://www.loc.gov/search/?q=' + queryParameter + '&fo=json';
+'https://www.loc.gov/' + format + '/?q=' + queryParameter + '&fo=json';
 
 var textInputEl = document.querySelector('#textInput');
 var selectEl = document.querySelector('#select');
-// var nameInputEl = document.querySelector('#username');
-// var repoContainerEl = document.querySelector('#repos-container');
-// var repoSearchTerm = document.querySelector('#repo-search-term');
 
-var formSubmitHandler = function (event) {
-  event.preventDefault();
 
-  var citySearch = disabledTextInputEl.value.trim();
+var getApi = function(queryParameter){
+    var requestUrl = 'https://www.loc.gov/search/?q=' + queryParameter + '&fo=json';
 
-  if (citySearch) {
-    getUserRepos(citySearch);
-
-    repoContainerEl.textContent = '';
-    nameInputEl.value = '';
-  } else {
-    alert('Please enter a GitHub username');
-  }
-};
-
-var buttonClickHandler = function (event) {
-  var language = event.target.getAttribute('data-language');
-
-  if (language) {
-    getFeaturedRepos(language);
-
-    repoContainerEl.textContent = '';
-  }
-};
-
-var getUserRepos = function (user) {
-  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
-
-  fetch(apiUrl)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          displayRepos(data, user);
+    fetch(requestUrl).then(function (response) {
+        if (response.ok){
+            response.json().then(function (data) {
+                displayData(data, queryParameter);
+        })
+        .catch(function (error) {
+        alert('Error' + response.statusText);
+        console.log(error);
         });
-      } else {
-        alert('Error: ' + response.statusText);
-      }
-    })
-    .catch(function (error) {
-      alert('Unable to connect to GitHub');
-    });
-};
-
-var getFeaturedRepos = function (language) {
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    return;
-  }
-
-  repoSearchTerm.textContent = searchTerm;
-
-  for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-    var repoEl = document.createElement('div');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
-};
-
-userFormEl.addEventListener('submit', formSubmitHandler);
-languageButtonsEl.addEventListener('click', buttonClickHandler);
+    }});
+}
