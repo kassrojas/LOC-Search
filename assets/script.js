3a130a8866ac373c56c3a2ba091706931a7cf929
kassrojas <kassandrarojass@gmail.com>
2022-12-09 11:04:08 -0500

    got cards to show with title results

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,52 @@
 // 'https://www.loc.gov/search/?q=' + queryParameter + '&fo=json';
 // 'https://www.loc.gov/' + format + '/?q=' + queryParameter + '&fo=json';
 
-var textInputEl = document.querySelector('#textInput');
-var selectEl = document.querySelector('#select');
-var submitButtonEl = document.querySelector('#submitButton');
-var goBackButton = document.querySelector('#goBackButton');
+var textInputEl = $('#textInput');
+var selectEl = $('#select');
+var submitButtonEl = $('#submitButton');
+var goBackButton = $('#goBackButton');
+var cardEl = $('.card');
+var cardContainer = $('.card-container');
 
-var getApi = function(queryParameter){
+var getApi = function (event) {
+    event.preventDefault();
+    var queryParameter = textInputEl.value;
+
     var requestUrl = 'https://www.loc.gov/search/?q=' + queryParameter + '&fo=json';
 
-    fetch(requestUrl).then(function (response) {
-        if (response.ok){
-            response.json().then(function (data) {
-                console.log(data + "data");
-                displayData(data, queryParameter);
-            })
+    fetch(requestUrl)
+        .then(function (response) {
+            if (response.ok) {
+                response.json().then(function (data) {
+                    console.log(data);
+                    for(i in data.results){
+                        // console.log(i + ". " +data.results[i].title);
+                        displayData(data.results[i]);
+                    }
+                })
+            }
+        }) 
         .catch(function (error) {
             alert('Error' + response.statusText);
             console.log(error);
         });
-        }
-    });
 };
 
 
-var displayData = function(){
+var displayData = function (result) {
+    var card = $("<div>");
+    card.addClass("card");
 
-    
+    var cardBody = $("<div>");
+    cardBody.addClass("card-body");
+
+    var cardTitle = $("<div>");
+    cardTitle.addClass("card-title");
+    cardTitle.textContent = result.title;
+
+    cardBody.append(cardTitle);
+    card.append(cardBody);
+    cardContainer.append(card);
 };
+
+submitButtonEl.on('click', getApi);
